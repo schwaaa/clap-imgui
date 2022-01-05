f627a33908ac6085e3bccbb593084877a96960f0
John Schwartz <schwa@cockos.com>
2022-01-05 16:47:22 -0500

    limping on mac

--- src/clap_imgui_mac.mm
+++ src/clap_imgui_mac.mm
@@ -1,9 +1,9 @@
-#import <Metal/Metal.h>
-#import <QuartzCore/QuartzCore.h>
+#include <Cocoa/Cocoa.h>
+#include <sys/time.h>
 
-#include "imgui/backends/imgui_impl_osx.h"
+#include "imgui_internal.h" // so we can get the viewport associated with an ImGui window
 #include "imgui/backends/imgui_impl_glfw.h"
-#include "imgui/backends/imgui_impl_metal.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 
 #define GLFW_INCLUDE_NONE
 #define GLFW_EXPOSE_NATIVE_COCOA
@@ -13,58 +13,117 @@
 #include "main.h"
 
 
-@interface ui_ctx_rec : NSObject
+@class imgui__timer;
+imgui__timer *timer;
+GLFWwindow *backend_wnd;
+unsigned int want_teardown;
+
+struct ui_ctx_rec
 {
-@public
   Plugin *plugin;
-  GLFWwindow *glfw_win;
-  id <MTLCommandQueue> cmd_q;
-  MTLRenderPassDescriptor *render_pass;
-  NSTimer *timer;
-}
--(void)imgui__on_timer:(id)sender;
-@end
+  NSView *parent;
+  char name[64];
+  int did_parenting;
+  ui_ctx_rec *next;
+};
+ui_ctx_rec *rec_list;
 
-@implementation ui_ctx_rec
--(void)imgui__on_timer:(id)sender
+void imgui__do_render_pass()
 {
-  @autoreleasepool
+  if (!backend_wnd) return;
+
+ // glfwPollEvents();
+
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+
+  ui_ctx_rec *rec=rec_list;
+  while (rec)
   {
-    glfwPollEvents();
-
-    int w, h;
-    glfwGetFramebufferSize(glfw_win, &w, &h);
-
-    NSWindow *nswin = glfwGetCocoaWindow(glfw_win);
-    CAMetalLayer *layer = (CAMetalLayer*)nswin.contentView.layer;
-    layer.drawableSize = CGSizeMake(w, h);
-    // or use GetIO().DisplaySize and layer.contentsScale ?
-    id <CAMetalDrawable> drawable = [layer nextDrawable];
-
-    render_pass.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0); // neededrender_pass
-    render_pass.colorAttachments[0].texture = drawable.texture;
-    render_pass.colorAttachments[0].loadAction = MTLLoadActionClear;
-    render_pass.colorAttachments[0].storeAction = MTLStoreActionStore;
-
-    id <MTLCommandBuffer> cmd_buf = [cmd_q commandBuffer];
-    id <MTLRenderCommandEncoder> enc = [cmd_buf renderCommandEncoderWithDescriptor:render_pass];
-
-    ImGui_ImplMetal_NewFrame(render_pass);
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-
-    ImGui::Begin("Clap Example", NULL, ImGuiWindowFlags_NoDecoration|ImGuiWindowFlags_NoResize);
-    plugin->plugin_impl__draw();
+    NSRect vr = [rec->parent convertRect:[rec->parent bounds] toView:nil];
+    NSRect wr = [[rec->parent window] convertRectToScreen:vr];
+    wr.origin.y = CGDisplayBounds(CGMainDisplayID()).size.height-(wr.origin.y+wr.size.height);
+    ImGui::SetNextWindowPos(ImVec2(wr.origin.x, wr.origin.y));
+    ImGui::SetNextWindowSize(ImVec2(wr.size.width, wr.size.height));
+    ImGui::Begin(rec->name, NULL,
+      ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking);
+    rec->plugin->plugin_impl__draw();
     ImGui::End();
 
-    ImGui::Render();
-    ImGui_ImplMetal_RenderDrawData(ImGui::GetDrawData(), cmd_buf, enc);
+    rec=rec->next;
+  }
 
-    [enc endEncoding];
-    [cmd_buf presentDrawable:drawable];
-    [cmd_buf commit];
+  ImGui::Render();
+  ImGui::UpdatePlatformWindows();
+  ImGui::RenderPlatformWindowsDefault();
+
+  rec=rec_list;
+  while (rec)
+  {
+    if (!rec->did_parenting)
+    {
+      extern ImGuiContext *GImGui;
+      for (int i=0; i < GImGui->Windows.Size; ++i)
+      {
+        ImGuiWindow *w=GImGui->Windows[i];
+        if (w->Name && !strcmp(w->Name, rec->name) &&
+          w->Viewport && w->Viewport->PlatformWindowCreated)
+        {
+          GLFWwindow *glfw_win=(GLFWwindow*)w->Viewport->PlatformHandle;
+          NSWindow *new_win=(NSWindow*)glfwGetCocoaWindow(glfw_win);
+          if (new_win) // assert
+          {
+            [[rec->parent window] addChildWindow:new_win ordered:NSWindowAbove];
+            rec->did_parenting=1;
+          }
+          break;
+        }
+      }
+    }
+    rec=rec->next;
+  }
+}
+
+@interface imgui__timer : NSObject
+{
+@public
+  NSTimer *timer;
+}
+-(void)on_timer:(id)sender;
+@end
+
+void imgui__teardown()
+{
+  [timer->timer invalidate];
+  [timer release];
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(backend_wnd); // will destroy this nsview
+  backend_wnd=NULL;
+  glfwTerminate();
+}
+
+unsigned int get_tick_count()
+{
+  struct timeval tm = {0};
+  gettimeofday(&tm, NULL);
+  return (unsigned int)(tm.tv_sec*1000 + tm.tv_usec/1000);
+}
+
+@implementation imgui__timer
+-(void)on_timer:(id)sender
+{
+  if (want_teardown > 0)
+  {
+    if (get_tick_count() > want_teardown) imgui__teardown();
+  }
+  else
+  {
+    imgui__do_render_pass();
   }
 }
 @end
@@ -79,153 +138,50 @@ bool Plugin::imgui__attach(void *parent)
   if (!parent) return false;
   if (m_ui_ctx) return true;
 
-  glfwSetErrorCallback(glfw_error_callback);
-  if (!glfwInit()) return false;
+  want_teardown=0;
+  if (!backend_wnd)
+  {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) return false;
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  //glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-  //glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-  GLFWwindow *glfw_win=glfwCreateWindow(400, 400, "Clap Example", NULL, NULL);
-  if (!glfw_win) return false;
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    // invisible top level window
+    backend_wnd=glfwCreateWindow(1, 1, "ImGui Backend", NULL, NULL);
+    if (!backend_wnd) return false;
 
-  id <MTLDevice> device = MTLCreateSystemDefaultDevice();
-  id <MTLCommandQueue> cmd_q = [device newCommandQueue];
-  CAMetalLayer *layer = [CAMetalLayer layer];
-  layer.device = device;
-  layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
-  layer.opaque = YES;
+    glfwMakeContextCurrent(backend_wnd);
+    glfwSwapInterval(1);
 
-  NSWindow *nswin = glfwGetCocoaWindow(glfw_win);
-  nswin.contentView.layer = layer;
-  nswin.contentView.wantsLayer = YES;
-  //[(NSView*)parent addSubview:nswin.contentView];
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    ImGui::GetIO().ConfigViewportsNoAutoMerge=true;
+    ImGui::GetIO().ConfigViewportsNoTaskBarIcon=true;
+    ImGui::GetIO().ConfigViewportsNoDefaultParent=true;
 
-  // retain this because imgui caches the render pipeline state for each render pass descriptor
-  MTLRenderPassDescriptor *render_pass = [MTLRenderPassDescriptor new];
+    ImGui_ImplGlfw_InitForOpenGL(backend_wnd, true);
+    ImGui_ImplOpenGL3_Init(NULL);
 
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGui::StyleColorsDark();
+    timer = [imgui__timer new];
+    timer->timer = [NSTimer scheduledTimerWithTimeInterval:0.030
+      target:timer selector:@selector(on_timer:) userInfo:nil repeats:YES];
+  }
 
-  ImGui_ImplGlfw_InitForOpenGL(glfw_win, true); // needed for metal too
-  ImGui_ImplMetal_Init(device);
+  ui_ctx_rec *new_rec=(ui_ctx_rec*)malloc(sizeof(ui_ctx_rec));
+  memset(new_rec, 0, sizeof(ui_ctx_rec));
+  new_rec->plugin = this;
+  new_rec->parent = (NSView*)parent;
+  sprintf(new_rec->name, "ImGui %p", this);
+  m_ui_ctx = new_rec;
 
-  ui_ctx_rec *rec = [ui_ctx_rec new];
-  rec->plugin=this;
-  rec->glfw_win = glfw_win;
-  rec->cmd_q = cmd_q;
-  rec->render_pass = render_pass;
-
-  rec->timer = [NSTimer scheduledTimerWithTimeInterval:0.030
-    target:rec selector:@selector(imgui__on_timer:) userInfo:nil repeats:YES];
-
-  return rec;
-}
-
-void Plugin::imgui__destroy()
-{
-  if (!m_ui_ctx) return;
-
-  ui_ctx_rec *rec=(ui_ctx_rec*)m_ui_ctx;
-  m_ui_ctx=NULL;
-
-  [rec->timer invalidate];
-  // release rec->blah
-  [rec release];
-
-  ImGui_ImplMetal_Shutdown();
-}
-
-
-////////////////////////////////
-
-#if 0 // opengl
-
-
-#include <OpenGL/gl.h>
-
-@interface ui_ctx_rec : NSObject
-{
-@public
-  Plugin *plugin;
-  GLFWwindow *glfw_win;
-  NSTimer *timer;
-}
--(void)imgui__on_timer:(id)sender;
-@end
-
-@implementation ui_ctx_rec
--(void)imgui__on_timer:(id)sender
-{
-  //glfwPollEvents();
-
-  int w, h;
-  glfwGetFramebufferSize(glfw_win, &w, &h);
-
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-  ImGui::NewFrame();
-  ImGui::SetNextWindowPos(ImVec2(0, 0));
-  ImGui::SetNextWindowSize(ImVec2(w, h));
-
-  ImGui::Begin("Clap Example", NULL, ImGuiWindowFlags_NoDecoration|ImGuiWindowFlags_NoResize);
-  plugin->plugin_impl__draw();
-  ImGui::End();
-
-  ImGui::Render();
-  glViewport(0, 0, w, h);
-  glClearColor(0.0, 0.0, 0.0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT);
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-  glfwSwapBuffers(glfw_win);
-}
-@end
-
-static void glfw_error_callback(int error, const char* description)
-{
-  fprintf(stderr, "GLFW error %d: %s\n", error, description);
-}
-
-bool Plugin::imgui__attach(void *parent)
-{
-  if (!parent) return false;
-  if (m_ui_ctx) return true;
-
-  glfwSetErrorCallback(glfw_error_callback);
-  if (!glfwInit()) return false;
-
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGui::StyleColorsDark();
-
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-  //glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-  GLFWwindow *glfw_win=glfwCreateWindow(400, 400, "Clap Example", NULL, NULL);
-  if (!glfw_win) return false;
-
-  ImGui_ImplGlfw_InitForOpenGL(glfw_win, true);
-  ImGui_ImplOpenGL3_Init(NULL);
-
-/*
-  HWND hwnd=glfwGetWin32Window(glfw_win);
-  SetParent(hwnd, (HWND)parent);
-  long style=GetWindowLong(hwnd, GWL_STYLE);
-  style &= ~WS_POPUP;
-  style |= WS_CHILDWINDOW;
-  SetWindowLong(hwnd, GWL_STYLE, style);
-  ShowWindow(hwnd, SW_SHOW);
-*/
-  ui_ctx_rec *rec = [ui_ctx_rec new];
-  rec->plugin=this;
-  rec->glfw_win = glfw_win;
-
-  rec->timer = [NSTimer scheduledTimerWithTimeInterval:0.030
-    target:rec selector:@selector(imgui__on_timer:) userInfo:nil repeats:YES];
+  if (rec_list) new_rec->next=rec_list;
+  rec_list=new_rec;
 
   return true;
 }
@@ -234,13 +190,22 @@ void Plugin::imgui__destroy()
 {
   if (!m_ui_ctx) return;
 
-  ui_ctx_rec *rec=(ui_ctx_rec*)m_ui_ctx;
+  ui_ctx_rec *old_rec=(ui_ctx_rec*)m_ui_ctx;
   m_ui_ctx=NULL;
 
-  [rec->timer invalidate];
-  free(rec);
+  ui_ctx_rec *prev_rec=NULL, *rec=rec_list;
+  while (rec)
+  {
+    if (rec == old_rec)
+    {
+      if (!prev_rec) rec_list=old_rec->next;
+      else prev_rec->next=old_rec->next;
+      break;
+    }
+    prev_rec=rec;
+    rec=rec->next;
+  }
+  free(old_rec);
 
-  ImGui_ImplOpenGL3_Shutdown();
+  if (!rec_list) want_teardown = get_tick_count()+1000;
 }
-
-#endif
