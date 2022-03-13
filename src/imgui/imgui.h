98e6376622cbc882e17645c11c4c48e9f868e90b
John Schwartz <schwa@cockos.com>
2022-03-13 09:01:03 -0400

    de-c++11

--- src/imgui/imgui.h
+++ src/imgui/imgui.h
@@ -42,6 +42,8 @@ Index of this file:
 
 #pragma once
 
+#define constexpr
+
 // Configuration file with compile-time options (edit imconfig.h or '#define IMGUI_USER_CONFIG "myfilename.h" from your build system')
 #ifdef IMGUI_USER_CONFIG
 #include IMGUI_USER_CONFIG
